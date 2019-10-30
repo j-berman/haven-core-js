65e43a9b9833e937305dd73b14789f2f9c51cd9a
dev <juhu>
2019-10-30 14:03:03 +0100

    update submodule

--- index.js
+++ index.js
@@ -33,8 +33,8 @@
 const mymonero_core_js = {};
 mymonero_core_js.monero_utils_promise = require("./monero_utils/MyMoneroCoreBridge")(); // NOTE: This is actually a promise. Call .then(function(monero_utils) { }) to actually use
 mymonero_core_js.monero_config = require("./monero_utils/monero_config");
-mymonero_core_js.monero_txParsing_utils = require("./monero_utils/monero_txParsing_utils");
-mymonero_core_js.monero_sendingFunds_utils = require("./monero_utils/monero_sendingFunds_utils");
+// mymonero_core_js.monero_txParsing_utils = require("./monero_utils/monero_txParsing_utils");
+// mymonero_core_js.monero_sendingFunds_utils = require("./monero_utils/monero_sendingFunds_utils");
 mymonero_core_js.monero_keyImage_cache_utils = require("./monero_utils/monero_keyImage_cache_utils");
 mymonero_core_js.monero_paymentID_utils = require("./monero_utils/monero_paymentID_utils");
 mymonero_core_js.monero_amount_format_utils = require("./monero_utils/monero_amount_format_utils");
